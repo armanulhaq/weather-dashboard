8a98881dfa785a994d1a42b5cee42206a165c026
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-10-05 14:14:31 +0530

    final changes

--- backend.py
+++ backend.py
@@ -11,12 +11,4 @@ def get_data(place, forecast_days=None, option=None):
     #which means 5 day data is given
     number_of_values_required = 8 * forecast_days
     filtered_data = filtered_data[:number_of_values_required]
-
-    if option=="Temperature":
-        filtered_data = [dict['main']['temp'] for dict in filtered_data]
-    if option =="Sky":
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
-
     return filtered_data
-if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, option='Temperature'))
--- main.py
+++ main.py
@@ -2,14 +2,35 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
-
 st.header("Weather Forecast for the Next Days")
 place = st.text_input("Place")
-days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forcasted days")
+days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days")
 option = st.selectbox("Select the data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+if place:
+    try:
+        with st.spinner("Fetching weather data..."):
+            filtered_data = get_data(place, days)
 
-figure = px.line(x=dates, y=temperatures, labels={"x":"Date", "y":"Temperature in C"})  #Creates a line graph
-st.plotly_chart(figure)
+            if option == "Temperature":
+                # Convert temperatures from Kelvin to Celsius
+                temperatures = [dict['main']['temp'] - 273.15 for dict in filtered_data]
+                dates = [dict['dt_txt'] for dict in filtered_data]
+                figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature in °C"})  # Creates a line graph
+                st.plotly_chart(figure)
+            else:
+                sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+                images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png", "Snow": "images/snow.png"}
+                image_paths = [images[condition] for condition in sky_conditions]
+                st.image(image_paths, width=115)
+    except KeyError:
+        st.write("Uh Oh! Such place doesn't exist in OpenWeather database")
+    except ConnectionError as ce:
+        st.error(str(ce))
+    except ValueError as ve:
+        st.error(str(ve))
+    except RuntimeError as re:
+        st.error(str(re))
+    except Exception as e:
+        st.error("Connection failed. The OpenWeather server might be unresponsive. Please refresh the page and try again.")
